aa31d2d6848b625432825f9fea54af194f219212
me-cafebabe <87921983+me-cafebabe@users.noreply.github.com>
2021-07-31 15:33:04 +0800

    sdm439-common: Import olive off-charging animation into vendor

--- sdm439-common-vendor.mk
+++ sdm439-common-vendor.mk
@@ -692,6 +692,31 @@ PRODUCT_COPY_FILES += \
     vendor/xiaomi/sdm439-common/proprietary/vendor/lib64/android.hardware.camera.provider@2.4-legacy.so:$(TARGET_COPY_OUT_VENDOR)/lib64/android.hardware.camera.provider@2.4-legacy.so \
     vendor/xiaomi/sdm439-common/proprietary/vendor/lib64/camera.device@3.2-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/camera.device@3.2-impl.so
 
+# Power-off charging
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/bin/chargeonlymode:$(TARGET_COPY_OUT_VENDOR)/bin/chargeonlymode \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/lib64/libsuspend.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libsuspend.so \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/0.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/0.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/10.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/10.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/1.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/1.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/2.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/2.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/3.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/3.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/4.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/4.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/5.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/5.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/6.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/6.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/7.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/7.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/8.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/8.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/9.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/9.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery_0.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_0.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/battery_fail.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/battery_fail.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery_green1.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_green1.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery_green2.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_green2.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery_green3.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_green3.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery_red1.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_red1.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/Battery_red2.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/Battery_red2.png \
+    vendor/xiaomi/sdm439-common/proprietary/vendor/res/charger/battery_scale.png:$(TARGET_COPY_OUT_VENDOR)/res/charger/battery_scale.png
+
 PRODUCT_PACKAGES += \
     libaudio_log_utils \
     libgpustats \
@@ -714,5 +739,4 @@ PRODUCT_PACKAGES += \
     qcrilmsgtunnel \
     WfdCommon \
     android.hardware.gnss@2.0-service-qti \
-    vendor.qti.gnss@3.0-service \
-    chargeonlymode
+    vendor.qti.gnss@3.0-service
